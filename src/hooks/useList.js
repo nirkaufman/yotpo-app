ec40fea60ef5a2b3e832dd11a45a14a98e24979c
nir.k <nir.k@next-insurance.com>
2022-06-08 14:49:32 +0300

    todolist

--- src/hooks/useList.js
+++ src/hooks/useList.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 
 export function useList(url) {
   const [items, setItems] = useState([]);
